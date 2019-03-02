760768c859038f6b871aef0f7c0c912410f0bba5
Alex1182-St <42576890+Alex1182-St@users.noreply.github.com>
2019-03-02 14:09:50 +0200

    02/03/2018

--- src/main/java/Lesson3/Methods/ArrElementsAverage.java
+++ src/main/java/Lesson3/Methods/ArrElementsAverage.java
@@ -6,15 +6,16 @@ public class ArrElementsAverage {
 
     public int arrAverage (ArrayList<Integer> arr) {
 
-        int average = 0;
+        int average;
         int arrayLength;
         int sum = 0;
 
         for (int i=0; i <arr.size(); i++){
             sum = sum + arr.get(i);
-            arrayLength = arr.size();
-            average = sum / arrayLength;
-        }
+                    }
+        arrayLength = arr.size();
+        average = sum / arrayLength;
+
         return average;
 
     }
