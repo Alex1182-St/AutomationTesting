824ca26a0a078a04ef48d73d74e77a01e91c54a7
Alex1182-St <42576890+Alex1182-St@users.noreply.github.com>
2019-03-04 21:56:38 +0200

    03/03/2018 - 19 задач готово

---
+++ src/main/java/Lesson3/Methods/AdjacentDigits.java
@@ -0,0 +1,42 @@
+package Lesson3.Methods;
+
+public class AdjacentDigits {
+
+    public int adjDigsProduct (String number, int digsQty) {
+
+
+        int maxProduct = 0;
+        /*
+        1 - перевести строку в масив м1 -  String to char
+        2 - створити масив м2 на основі м1 без нулів -
+        щось на зразок
+               int [] m2 = new int [];
+               int j = 0;
+
+        for (int i = 0; i < ; i++) {
+             if(m1[i]!= 0){
+                m2[j] = m1[i];
+                index++;
+            } else {
+            continue
+            }
+
+         */
+
+
+
+
+        for (int i = 0; i < number.length() - digsQty ; i++) {
+
+
+
+        }
+
+
+
+
+
+    }
+
+
+}
