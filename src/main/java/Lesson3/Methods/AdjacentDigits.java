8c81b66976c5f2669c8984a122005f1352f9512e
Alex1182-St <alexander.stepaniuk@yahoo.com.ua>
2019-03-05 13:18:54 +0200

    05/03/2019 - 20 задач готово.

--- src/main/java/Lesson3/Methods/AdjacentDigits.java
+++ src/main/java/Lesson3/Methods/AdjacentDigits.java
@@ -2,41 +2,28 @@ package Lesson3.Methods;
 
 public class AdjacentDigits {
 
-    public int adjDigsProduct (String number, int digsQty) {
+    public void adjDigsProduct (String number, int digsQty) {
 
-
-        int maxProduct = 0;
-        /*
-        1 - перевести строку в масив м1 -  String to char
-        2 - створити масив м2 на основі м1 без нулів -
-        щось на зразок
-               int [] m2 = new int [];
-               int j = 0;
-
-        for (int i = 0; i < ; i++) {
-             if(m1[i]!= 0){
-                m2[j] = m1[i];
-                index++;
-            } else {
-            continue
-            }
-
-         */
-
-
-
+        long maxProduct = 0;
+        String strWithMaxProduct = null;
 
         for (int i = 0; i < number.length() - digsQty ; i++) {
 
+        int product = 1;
+        for (int j = 0; j < digsQty; j ++) {
 
+            if (Character.getNumericValue(number.charAt(i + j))!=0) {
+            product = product * Character.getNumericValue(number.charAt(i + j)); // увага на цей момент - i + j
+            }
 
+            if (product > maxProduct) {
+                maxProduct = product;
+                strWithMaxProduct = number.substring(i, i+digsQty);
+
+            }
         }
-
-
-
-
-
-    }
-
-
-}
+        }
+        System.out.println("\n Max product of adjacent is " + maxProduct);
+        System.out.println("\n Adjacement digits are " + strWithMaxProduct);
+        }
+        }
