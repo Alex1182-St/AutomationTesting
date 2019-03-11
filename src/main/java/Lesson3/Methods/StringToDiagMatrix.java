9bc79bd895d0c94478e121da0798293a5ebc1acd
Alex1182-St <alexander.stepaniuk@yahoo.com.ua>
2019-03-11 09:27:19 +0200

    11.03.2019 - 24 задачі

--- src/main/java/Lesson3/Methods/StringToDiagMatrix.java
+++ src/main/java/Lesson3/Methods/StringToDiagMatrix.java
@@ -6,17 +6,15 @@ public class StringToDiagMatrix {
 
         char [] fromString = ourString.toCharArray();
 
-        String s = "s";
+        String s = " ";
+        String w = "  ";
 
         for (int i = 0; i < fromString.length ; i++) {
 
             System.out.println(s + fromString[i]);
 
+            s = s + w;
 
         }
-
-
     }
-
-
 }
