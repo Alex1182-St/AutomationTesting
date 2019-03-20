ad0b5670ba7254dc76c3e70595176054f7bd6f3b
Alex1182-St <alexander.stepaniuk@yahoo.com.ua>
2019-03-20 13:59:48 +0200

    Додав xml-файл для запуску тест-кейсів (TestNG_Suite.xml).

--- src/main/java/Lesson3/A1_Methods/TemperatureConverter.java
+++ src/main/java/Lesson3/A1_Methods/TemperatureConverter.java
@@ -17,17 +17,21 @@ public class TemperatureConverter {
             System.out.println("\nEnter your temperature in Kelvin. Use numbers only");
             double kelv = sc.nextDouble();
             convertedTemp = kelv - 273.15;
+            return convertedTemp;
+
         }
         else if (choise.equals("k")) {
             System.out.println("\nEnter your temperature in Celcium. Use numbers only");
             double celc = sc.nextDouble();
             convertedTemp = celc + 273.15;
+            return convertedTemp;
         }
         else {
             System.out.println("Celcium of Kelvin has not been chosen. Run the program again");
+            return tempConvert();
         }
-        sc.close();
-        return convertedTemp;
+
+
     }
 
 
